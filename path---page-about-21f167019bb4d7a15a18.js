c103d3f3bfdec51c2dcf7fe6d0dedd2cfbc1fe64
akaguny <alexeykolpino@gmail.com>
2018-01-30 22:38:55 +0300

    Updates

--- path---page-about-21f167019bb4d7a15a18.js
+++ path---page-about-21f167019bb4d7a15a18.js
@@ -1,2 +1,2 @@
-webpackJsonp([0x609ab029c0bc],{535:function(t,a){t.exports={data:{markdownRemark:{html:'<p>Студент ГУАП\n— [url=https://jsfiddle.net/user/Quncore/fiddles/]jsfiddle[/url]\n— [url=https://github.com/akaguny]github<a href="">/url</a>Моё резюме[/url]</p>',frontmatter:{date:"2015-09-25",path:"/page/about",title:"О сайте"}}},pathContext:{}}}});
+webpackJsonp([0x609ab029c0bc],{538:function(t,a){t.exports={data:{markdownRemark:{html:'<p>Студент ГУАП\n— [url=https://jsfiddle.net/user/Quncore/fiddles/]jsfiddle[/url]\n— [url=https://github.com/akaguny]github<a href="">/url</a>Моё резюме[/url]</p>',frontmatter:{date:"2015-09-25",path:"/page/about",title:"О сайте"}}},pathContext:{}}}});
 //# sourceMappingURL=path---page-about-21f167019bb4d7a15a18.js.map
